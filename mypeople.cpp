13128783a35c16e03831a602297017e6df9c7177
Adriano Naime Bonin <adriano.bonin@outlook.com>
2021-12-24 16:08:32 -0300

    Created the insertion method for getting the info inputed. Display yet to be developed

--- mypeople.cpp
+++ mypeople.cpp
@@ -1,7 +1,16 @@
 //  Copyright 2021 c3nt4ur1 (Adriano Naime Bonin)
 
+#include <iostream>
+#include <vector>
+#include "qdebug.h"
 #include "mypeople.h"
 #include "./ui_mypeople.h"
+using namespace std;
+
+vector<QString> names;
+vector<QString> arrival_hour;
+static int total_guests = 0;
+
 
 mypeople::mypeople(QWidget *parent)
     : QMainWindow(parent)
@@ -19,5 +28,14 @@ mypeople::~mypeople()
 void mypeople::on_pb_exit_clicked()
 {
     close();
+}
+
+
+void mypeople::on_pb_add_clicked()
+{
+    names.push_back(ui->le_name->text());
+    arrival_hour.push_back(QString::number(ui->te_arrival->time().hour()) + QString::number(ui->te_arrival->time().minute()));
+    ++total_guests;
+
 }
 
